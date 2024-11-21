3e3e61b74078ec1df3be4dc50bbf508d3a310a33
Wladimir Kuhn <vovadym13@gmail.com>
2024-11-21 19:26:10 +0300

    Copleted the tasks as part of the first homework

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9,10);
+        getAverage(numbers);
+    }
+
+    public static void getAverage (List<Integer> numbers) {
+
+        int sum = numbers.stream()
+                .filter(number -> number % 2 == 0).mapToInt(Integer::intValue)
+                .sum();
+
+        double count = numbers.stream()
+                .filter(number -> number % 2 == 0).count();
+
+        System.out.println("Среднее арифметическое четных чисел в списке =" + " "+sum / count);
+    }
+}
